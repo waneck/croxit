9c3e33e96e40d291cfb5311a17b204eb7b00789a
Caue Waneck <waneck@gmail.com>
2012-08-30 02:59:07 -0300

    First example test

---
+++ haxelib/examples/01 - Hello World/Main.hx
@@ -0,0 +1,16 @@
+class Main
+{
+	
+	public static function main()
+	{
+		croxit.Output.println('
+		<html>
+			<head>
+				<title>Hello World Title</title>
+			</head>
+			<body>
+			<h1>Hello, World!</h1>
+		</html>');
+	}
+	
+}
