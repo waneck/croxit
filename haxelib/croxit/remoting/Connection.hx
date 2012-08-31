1d6607d34e2b87c4bd3da4e723d0ad09485e4f78
Cauê Waneck <waneck@gmail.com>
2012-08-31 16:09:31 -0300

    Added compatibility layer for server-side remoting request handling

--- haxelib/croxit/remoting/Connection.hx
+++ haxelib/croxit/remoting/Connection.hx
@@ -1,62 +1,65 @@
 package croxit.remoting;
-import croxit.core.Events;
 import haxe.remoting.Context;
 import haxe.Unserializer;
+#if iphone
+import croxit.core.Events;
 import croxit.Croxit;
 import haxe.Serializer;
+
+#end
 
 #if !js
 //js can only call asynchronously
 
-class Connection implements haxe.remoting.Connection
+class Connection
 {
 	static var __ctx:Context = new Context();
 	
-	var __path : Array<String>;
-	
-	function new(path) : Void 
-	{
-		this.__path = path;
-	}
-	
-	private static function __init__():Void
-	{
-		Events.addHandler("cxconnect", doConnection);
-	}
-	
-	public function call( params : Array<Dynamic> ) : Dynamic
-	{
-		var s = new Serializer();
-		s.serialize(__path);
-		s.serialize(params);
-		var p = s.toString();
-		var ret = Croxit.callJS("croxit.remoting.AsyncConnection.doConnection('" + StringTools.replace(p, "'", "\\'") + "');");
-		
-		if (ret == null)
-			return null;
-		return Unserializer.run(ret);
-	}
-	
-	public function resolve( name : String ) : haxe.remoting.Connection
-	{
-		var c = new Connection(__path.copy());
-		c.__path.push(name);
-		return c;
-	}
-	
-	public static function connect():haxe.remoting.Connection
-	{
-		return new Connection([]);
-	}
-	
+	/**
+	 *  Adds an object to the current remoting context
+	 **/
 	public static function addObject(name:String, obj:Dynamic, rec:Bool=false):Void
 	{
 		__ctx.addObject(name, obj, rec);
 	}
 	
+	/**
+	 *  Sets current remoting context
+	 **/
 	public static function setContext(ctx:Context):Void
 	{
 		__ctx = ctx;
+	}
+	
+	/**
+	 *  Provides a compatibility with Neko's HttpConnection:
+	   *  For Croxit targets, this is equivalent to using setContext, and will always return false
+	   *  For Web targets, it will handle the remoting request if possible,
+	   *    and will return if the request was succesfully handled.
+	 **/
+	public static function handleRequest(?ctx:Context):Bool
+	{
+		#if iphone
+		if (ctx != null)
+			setContext(ctx);
+		
+		return false;
+		
+		#else
+		return haxe.remoting.HttpConnection.handleRequest( (ctx != null) ? ctx : __ctx );
+		
+		#end
+	}
+	
+	#if iphone
+	public static function connect():haxe.remoting.Connection
+	{
+		return new Cnx([]);
+	}
+	
+	private static function __init__():Void
+	{
+		Events.addHandler("cxconnect", doConnection);
 	}
 	
 	private static function doConnection(request:String):Void
@@ -89,6 +92,41 @@ class Connection implements haxe.remoting.Connection
 		if (id != null && id >= 0)
 			Croxit.callJS('croxit.js.Client.doCall(' + id + ', "' + StringTools.replace(s.toString(), "'", "\\'") + '");');
 	}
+	
+	#end
 }
+
+#if iphone
+private class Cnx implements haxe.remoting.Connection
+{
+	var __path : Array<String>;
+	
+	function new(path) : Void 
+	{
+		this.__path = path;
+	}
+	
+	public function call( params : Array<Dynamic> ) : Dynamic
+	{
+		var s = new Serializer();
+		s.serialize(__path);
+		s.serialize(params);
+		var p = s.toString();
+		var ret = Croxit.callJS("croxit.remoting.AsyncConnection.doConnection('" + StringTools.replace(p, "'", "\\'") + "');");
+		
+		if (ret == null)
+			return null;
+		return Unserializer.run(ret);
+	}
+	
+	public function resolve( name : String ) : haxe.remoting.Connection
+	{
+		var c = new Connection(__path.copy());
+		c.__path.push(name);
+		return c;
+	}
+}
+
+#end
 
 #end
